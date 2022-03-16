789b5ad33f6d61c556c346ec53d34f9c29421164
gwan1 <jeonggwanil@gmail.com>
2022-03-16 10:34:54 +0900

    cahe_trace.txt

---
+++ trace/trace/trace.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX_SIZE 9000
+
+int main(void) {
+
+	FILE* fp = fopen("test_trace.txt", "wt");
+
+	if (fp != NULL) {
+		srand((unsigned int)time(NULL));
+
+		for (int i = 0; i < MAX_SIZE; i++) {
+			fprintf(fp, "%d, ", rand() % 301);
+		}
+		fclose(fp);
+	}
+	return 0;
+}
